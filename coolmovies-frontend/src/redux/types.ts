2e83705ac3bcec73785a4982f2cedf9bd4eebd11
Giordano Bruno <giordanosi2011@gmail.com>
2024-08-30 16:24:28 -0300

    Add typings to redux store

--- coolmovies-frontend/src/redux/types.ts
+++ coolmovies-frontend/src/redux/types.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
 
 export type CreateStoreOptions = {
   epicDependencies?: EpicDependencies;
@@ -6,4 +6,33 @@ export type CreateStoreOptions = {
 
 export type EpicDependencies = {
   client: ApolloClient<NormalizedCacheObject>;
+};
+
+export type MovieDetails = {
+  id: string;
+  title: string;
+  imgUrl: string;
+  releaseDate: string;
+};
+
+export type ReviewDetails = {
+  id: string;
+  body: string;
+  title: string;
+  rating: number;
+  nodeId: number;
+};
+
+export type CurrentUser = {
+  id: string;
+  name: string;
+};
+
+export type CreateReviewInputType = {
+  id?: string;
+  body: string;
+  rating: number;
+  title: string;
+  movieId: string;
+  userReviewerId?: string;
 };
